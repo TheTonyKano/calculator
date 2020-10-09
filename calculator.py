0500b9a03b9f01f2044da3e7b868345dcbe7f27d
TheTonyKano <70045901+TheTonyKano@users.noreply.github.com>
2020-10-09 17:36:16 -0400

    Update calculator.py
    Updated to type the selection add, subtract, etc.

--- calculator.py
+++ calculator.py
@@ -1,60 +1,69 @@
+num1 = ""
+num2 = ""
+total = ""
+
+
+def add(num1, num2):
+    global total
+    total = num1 + num2
+    return total
+
+
+def subtract(num1, num2):
+    global total
+    total = num1 - num2
+    return total
+
+
+def multiply(num1, num2):
+    global total
+    total = num1 * num2
+    return total
+
+
+def divide(num1, num2):
+    global total
+    total = num1 / num2
+    return total
+
+
+def user_input():
+    global num1
+    global num2
+    num1 = float(input("Please enter a number :"))
+    num2 = float(input("Please enter another number :"))
+    return num1, num2
+
 while True:
-	print ("If you want to add two numbers, please type 'add'")
-	print ("If you want to subtract two numbers, please type 'subtract'")
-	print ("If you want to divide two numbers, please type 'divide'")
-	print ("If you want to multiply two numbers, please type 'multiply'")
-	print ("If you want to end the program, please type 'quit'")
-	
-def add(x, y):
-        return x + y
+    print("If you want to add two numbers, please type 'add'")
+    print("If you want to subtract two numbers, please type 'subtract'")
+    print("If you want to divide two numbers, please type 'divide'")
+    print("If you want to multiply two numbers, please type 'multiply'")
+    print("If you want to end the program, please type 'quit'")
+    choice = input("Please type your choice: ")
 
-def subtract (x, y):
-        return x - y
-        
-def multiply(x, y):
-        return x * y
+    if choice == 'add':
+        user_input()
+        add(num1, num2)
+        print(num1, "+", num2, "=", total)
 
-def divide (x, y):
-        return x / y
-	
-choice = input("Enter choice(1/2/3/4: ")
-	
-if choice == '1':
-        print(num1, "+", num2, "=", add(num1,num2))
+    elif choice == 'subtract':
+        user_input()
+        subtract(num1, num2)
+        print(num1, "-", num2, "=", total)
 
-elif choice == '2':
-        print(num1, "-", num2, "=", subtract(num1,num2))
-        
-elif choice == '3':
-        print(num1, "*", num2, "=", multiply(num1,num2))
+    elif choice == 'multiply':
+        user_input()
+        multiply(num1, num2)
+        print(num1, "*", num2, "=", total)
 
-elif choice == '4':
-        print(num1, "/", num2, "=", divide(num1,num2))
+    elif choice == 'divide':
+        user_input()
+        divide(num1, num2)
+        print(num1, "/", num2, "=", total)
 
-def user_input()
-		num1 = float(input("Please enter a number :"))
-		num2 = float(input("Please enter another number :"))
-		result1 = str(num1 + num2)
-		print("The result is:" + result1)
-			
-def user_input()
-		num3 = float(input("Please enter a number :"))
-		num4 = float(input("Please enter another number :"))
-		result2 = str(num3 - num4)
-		print("The result is:" + result2)
-		
-def user_input():
-		num5 = float(input("Please enter a number :"))
-		num6 = float(input("Please enter another number :"))
-		result3 = str(num5 / num6)
-		print("The result is:" + result3)
-	
-def user_input():
-		num7 = float(input("Please enter a number :"))
-		num8 = float(input("Please enter another number :"))
-		result4 = str(num3 * num4)
-		print("The result is:" + result4)
-else:
-	print ("Unknown input")
-elif: 
-	print('You have not typed a valid operator, please run the program again.')
+    elif choice == "quit":
+        break
+
+    else:
+        print("Unknown input")
